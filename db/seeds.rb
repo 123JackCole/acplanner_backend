c81606d66e90f357b7b7b20211af2cbecb076b19
123JackCole <123jack.cole@gmail.com>
2020-04-29 10:17:48 -0700

    backend commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
+# User.destroy_all
 Collectable.destroy_all
 Bug.destroy_all
 Fish.destroy_all
@@ -14,6 +14,20 @@ Fossil.destroy_all
 Villager.destroy_all
 
 main_collection = Collectable.create( name: 'seed_collection' )
+
+ant = Bug.create(
+    collectable_id: main_collection.id,
+    name: "Ant",
+    image: "https://nookipedia.com/w/images/thumb/4/4a/Ant_NH.png/200px-Ant_NH.png",
+    scientific_name: "N/A",
+    family: "Formicidae - Ants",
+    time_year: "All year",
+    time_day: "All day",
+    location: "Discarded food, trash",
+    size: "5 mm",
+    rarity: "Common",
+    price: "80 Bells"
+)
 
 moth = Bug.create(
     collectable_id: main_collection.id,
@@ -44,15 +58,41 @@ sturgeon = Fish.create(
     shadow: "Huge"
 )
 
+tuna = Fish.create(
+    collectable_id: main_collection.id,
+    name: "Tuna",
+    image: "https://nookipedia.com/w/images/thumb/6/60/Tuna_NH.png/200px-Tuna_NH.png",
+    scientific_name: "Thunnus orientalis",
+    family: "Scombridae - Tuna",
+    time_year: "November to March",
+    time_day: "All day",
+    location: "OceanPier",
+    size: "225 cm",
+    rarity: "Rare",
+    price: "7,000 Bells",
+    shadow: "Huge"
+)
+
+Amber = Fossil.create(
+    collectable_id: main_collection.id,
+    name: "Amber",
+    image: "https://nookipedia.com/w/images/5/5a/Amber.jpg",
+    scientific_name: "Amber",
+    sections: "N/A",
+    period: "N/A",
+    length: "N/A",
+    price: "1,200 Bells"
+)
+
 Archelon = Fossil.create(
     collectable_id: main_collection.id,
     name: "Archelon",
     image: "https://nookipedia.com/w/images/thumb/7/70/Archelon.jpg/200px-Archelon.jpg",
     scientific_name: "Archelon ischyros",
-    sections: "Archelon SkullArchelon Torso",
+    sections: "Archelon Skull\nArchelon Torso",
     period: "Late Cretaceous",
     length: "13 feet (6.5 meters)",
-    price: "Skull: 4,000 BellsTorso: 3,500 Bells"
+    price: "Skull: 4,000 Bells\nTorso: 3,500 Bells"
 )
 
 static = Villager.create(
@@ -73,4 +113,24 @@ static = Villager.create(
     favclothing: "N/A",
     leastfavclothing: "N/A",
     favcolor: "N/A"
+)
+
+tia = Villager.create(
+    name: "Tia",
+    image: "https://nookipedia.com/w/images/thumb/6/67/Tia_NL.png/200px-Tia_NL.png",
+    quote: "Tea is a cup of life.",
+    gender: "Female",
+    personality: "Normal",
+    species: "Elephant",
+    birthday: "November 18th",
+    sign: "Scorpio",
+    phrase: "teacup",
+    clothes: "Garden Tank",
+    picture: "https://nookipedia.com/w/images/1/1d/TiaPicACNL.png",
+    siblings: "Fourth of six",
+    goal: "Dentist",
+    fear: "Skeleton Hood",
+    favclothing: "Cute",
+    leastfavclothing: "Rock 'n' Roll",
+    favcolor: "Beige"
 )
