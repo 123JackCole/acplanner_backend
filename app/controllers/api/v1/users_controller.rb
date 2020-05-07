de1f7da9b5b2501b6b9160bfb89ca544f3a17e21
123JackCole <123jack.cole@gmail.com>
2020-05-07 11:08:13 -0700

    now correctly creates checked boxes array upon user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,55 +1,62 @@
 class Api::V1::UsersController < ApplicationController
-    skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: [:create]
 
-    def index
-        @users = User.all
-        render json: @users
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    render json: { user: StudentSerializer.new(currentUser) }
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      @dailychecklist = Dailychecklist.create(user_id: @user.id, checked_statuses: "
+        [false,false,false,false,false,false],
+        [false,false,false],
+        [false,false],
+        [false],
+        [false,false],
+        [false,false,false,false,false],
+        [false,false,false,false,false,false,false,false,false,false]
+      ")
+      Task.create(dailychecklist_id: @dailychecklist.id, name: "Hit Rocks", num_of_boxes: 6)
+      Task.create(dailychecklist_id: @dailychecklist.id, name: "Excavate Fossils", num_of_boxes: 4)
+      Task.create(dailychecklist_id: @dailychecklist.id, name: "Plant/Harvest Money Tree", num_of_boxes: 2)
+      Task.create(dailychecklist_id: @dailychecklist.id, name: "Find Recipe Bottle on Beach", num_of_boxes: 1)
+      Task.create(dailychecklist_id: @dailychecklist.id, name: "Furniture From Trees", num_of_boxes: 2)
+      Task.create(dailychecklist_id: @dailychecklist.id, name: "Wasps From Trees", num_of_boxes: 5)
+      Task.create(dailychecklist_id: @dailychecklist.id, name: "Coins From Trees", num_of_boxes: 10)
+      # Calendar.create(user_id: @user.id)
+      token = encode_token(@user)
+      render json: { user: UserSerializer.new(@user), jwt: token }
+    else
+      render json: { error: @user.errors.full_messages }
     end
+  end
 
-    def show
-        render json: {user: StudentSerializer.new(currentUser)}
-    end
+  # def update
+  #     @user = User.find(params[:id])
+  #     if @user.valid?
+  #         @user.update(user_params)
+  #         render json: @user
+  #     else
+  #         render json: { message: @user.errors.full_messages }
+  #     end
+  # end
 
-    def create
-        @user = User.new(user_params)
-        if @user.valid?
-            @user.save
-            @dailychecklist = Dailychecklist.create(user_id: @user.id, checked_statuses: [])
-            Task.create(dailychecklist_id: @dailychecklist.id, name: "Hit Rocks", num_of_boxes: 6)
-            Task.create(dailychecklist_id: @dailychecklist.id, name: "Excavate Fossils", num_of_boxes: 4)
-            Task.create(dailychecklist_id: @dailychecklist.id, name: "Plant/Harvest Money Tree", num_of_boxes: 2)
-            Task.create(dailychecklist_id: @dailychecklist.id, name: "Find Recipe Bottle on Beach", num_of_boxes: 1)
-            Task.create(dailychecklist_id: @dailychecklist.id, name: "Furniture From Trees", num_of_boxes: 2)
-            Task.create(dailychecklist_id: @dailychecklist.id, name: "Wasps From Trees", num_of_boxes: 5)
-            Task.create(dailychecklist_id: @dailychecklist.id, name: "Coins From Trees", num_of_boxes: 10) 
-            # Calendar.create(user_id: @user.id)
-            token = encode_token(@user)
-            render json: { user: UserSerializer.new(@user), jwt: token }
-        else 
-            render json: { error: @user.errors.full_messages} 
-        end
-    end
+  # def destroy
+  #     @user = User.find(params[:id])
+  #     @user.destroy
+  #     render json: { message: "User successfully deleted" }
+  # end
 
-    # def update
-    #     @user = User.find(params[:id])
-    #     if @user.valid?
-    #         @user.update(user_params)
-    #         render json: @user
-    #     else
-    #         render json: { message: @user.errors.full_messages }
-    #     end
-    # end
+  private
 
-    # def destroy
-    #     @user = User.find(params[:id])
-    #     @user.destroy
-    #     render json: { message: "User successfully deleted" }
-    # end
-
-    private
-
-    def user_params
-        params.require(:user).permit(:username, :password)
-    end
-
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
